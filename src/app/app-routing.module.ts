c807c90961317889c4b6128e4a6d7d9490854e3b
Vamsikrishna Boligirla <Vamsikrishna.Boligirla@citiustech.com>
2021-12-01 11:35:48 +0530

    nursedashboardstructure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { AddPhysicianComponent } from './Component/admin/add-physician/add-physician.component';
 import { AdminDashboardComponent } from './Component/admin/admin-dashboard/admin-dashboard.component';
-import { AdminModule } from './Component/admin/admin.module';
 import { ViewPhysicianComponent } from './Component/admin/view-physician/view-physician.component';
 import { HomeComponent } from './Component/home/home.component';
 import { LoginComponent } from './Component/login/login.component';
+import { NurseDashboardComponent } from './Component/nurse/nurse-dashboard/nurse-dashboard.component';
 //import { NavMenuComponent } from './Component/nav-menu/nav-menu.component';
 import { PhysicianComponent } from './Component/physician/physician.component';
 
@@ -44,6 +44,10 @@ const routes: Routes = [
   {
     component: ViewPhysicianComponent,
     path: 'ViewPhysician',
+  },
+  {
+    component: NurseDashboardComponent,
+    path: 'NurseDashboard',
   },
   { path: '**', redirectTo: '' },
 ];
