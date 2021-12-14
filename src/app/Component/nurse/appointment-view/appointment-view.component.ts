34b7f270bd1a02b6d6fbadbb45794b472761e6f8
Vamsikrishna Boligirla <Vamsikrishna.Boligirla@citiustech.com>
2021-12-14 16:21:57 +0530

    Grid Data Commit

--- src/app/Component/nurse/appointment-view/appointment-view.component.ts
+++ src/app/Component/nurse/appointment-view/appointment-view.component.ts
@@ -1,72 +1,57 @@
-import {Component, ViewChild,OnInit} from '@angular/core';
-import { MatSort} from '@angular/material/sort';
-
-import {MatPaginator} from '@angular/material/paginator';
-import {MatTableDataSource} from '@angular/material/table';
-import { Product,products } from 'src/app/models/appointment';
+import { Component, ViewChild, OnInit, ElementRef } from '@angular/core';
+import { MatSort } from '@angular/material/sort';
+import { MatPaginator } from '@angular/material/paginator';
 import { MatDialog } from '@angular/material/dialog';
 import { EditDailogeComponent } from '../dailoge/edit-dailoge/edit-dailoge.component';
-
-
+import { DailogeService } from 'src/app/Services/dailoge.service';
+import { Product } from 'src/app/models/appointment';
+import { MatTableDataSource } from '@angular/material/table';
 
 /**
  * @title Table with sorting
  */
- @Component({
+@Component({
   selector: 'app-appointment-view',
   templateUrl: './appointment-view.component.html',
   styleUrls: ['./appointment-view.component.css']
 })
-export class AppointmentViewComponent  {
-displayedColumns = ['userID','name', 'gender','address','mobile', 'age', 'bloodGroup' ,'temparature', 'bloodPresure', 'sugarLevel','edit','delete'];
-products:Product[]= products;
-dataSource = new MatTableDataSource(products);
-@ViewChild(MatPaginator) paginator !: MatPaginator;
-@ViewChild(MatSort, {}) sort !: MatSort;
+export class AppointmentViewComponent {
+  displayedColumns = ['id', 'name', 'gender', 'address', 'mobile', 'age', 'bloodGroup', 'temparature', 'bloodPresure', 'sugarLevel', 'edit', 'delete'];
+  dataSource1 !: MatTableDataSource<Product>;
+  @ViewChild(MatPaginator) paginator !: MatPaginator;
+  @ViewChild(MatSort, {}) sort !: MatSort;
 
+  constructor(public dialogService: MatDialog, public appoiService: DailogeService) { }
 
-constructor(
-  public dialogService: MatDialog
-  ) {}
-  /**
-   * Set the sort after the view init since this component will
-   * be able to query its view for the initialized sort.
-   */
-  ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
+  ngOnInit() {
+    this.getdata();
   }
-
-
-   ngOnInit() {
-    //this.dataSource = this.products;
-    //this.dataSource.paginator = this.paginator;
-
+  getdata() {
+    this.appoiService.getAppointmentData().subscribe(data => {
+      this.dataSource1 = new MatTableDataSource(data)    
+      this.dataSource1.paginator = this.paginator;
+    });
   }
-  startEdit(data:any[]){
-    console.log("edit worked",data)
+  startEdit(data: any[]) {    
     const dialogRef = this.dialogService.open(EditDailogeComponent, {
-      data: {data}
+      data: { data }
     });
     dialogRef.afterClosed()
   }
+  applyFilter(filterValue: any) {
+    let itemvalue = filterValue.target.value;   
+    this.dataSource1.filter = itemvalue.trim().toLowerCase();
+    this.dataSource1.paginator = this.paginator;
 
-  
-
-
-
-// filterProduct(value: string):void{
-//     this.dataSource.filter = value.trim().toLowerCase();
-//  this.serviceAPI.getDataByFilter(value).subscribe(response =>
-//  {
-//    this.dataSource= response['products'];
-
-//  });
-//   }
-
-  
+  }
+  onDelete(rowid: number) {
+    this.appoiService.deletePostapp(rowid);
+    this.getdata();
+  }
 
 }
+
+
 function ELEMENT_DATA(ELEMENT_DATA: any) {
   throw new Error('Function not implemented.');
 }
