50975c04356a325e7e475c41b7e1570fc802708d
hemanthgowdak <94034662+hemanthgowdak@users.noreply.github.com>
2022-01-03 23:54:38 +0530

    Revert "AdminPatient and Hospital and Password Changes"

--- src/app/Component/home/forgotpassword/forgotpassword.component.spec.ts
+++ src/app/Component/home/forgotpassword/forgotpassword.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { FooterComponent } from './footer.component';
+import { ForgotpasswordComponent } from './forgotpassword.component';
 
-describe('FooterComponent', () => {
-  let component: FooterComponent;
-  let fixture: ComponentFixture<FooterComponent>;
+describe('ForgotpasswordComponent', () => {
+  let component: ForgotpasswordComponent;
+  let fixture: ComponentFixture<ForgotpasswordComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ FooterComponent ]
+      declarations: [ ForgotpasswordComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(FooterComponent);
+    fixture = TestBed.createComponent(ForgotpasswordComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
