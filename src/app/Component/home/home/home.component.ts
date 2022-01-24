55367f0847761063014f0693804c497fed1fc97f
Hemanth K <hemanthk@citiustech.com>
2022-01-25 00:21:13 +0530

    Login Changes

--- src/app/Component/home/home/home.component.ts
+++ src/app/Component/home/home/home.component.ts
@@ -31,13 +31,13 @@ export class HomeComponent {
     this.loading = true;
     this.isLoggedIn$ = this.authenticationService.isLoggedIn; // {2}
     const user = this.currentUser;
-    if (user.role == 'Admin') {
+    if (user.role == 'ADMIN') {
       this.router.navigateByUrl('/AdminDashboard');
-    } else if (user.role == 'Physician') {
+    } else if (user.role == 'PHYSICIAN') {
       this.router.navigateByUrl('/');
-    } else if (user.role == 'Patient') {
+    } else if (user.role == 'PATIENT') {
       this.router.navigateByUrl('/PatientDashboard');
-    } else if (user.role == 'Nurse') {
+    } else if (user.role == 'NURSE') {
       this.router.navigateByUrl('/NurseDashboard');
     }
   }
